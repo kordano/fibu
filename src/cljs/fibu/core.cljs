ce19d92366a8118d0d574d4abf4c3332d99944ca
Konrad Kühne <konnykuehne@gmail.com>
2016-12-18 18:47:13 +0100

    refactor all

---
+++ src/cljs/fibu/core.cljs
@@ -0,0 +1,25 @@
+(ns fibu.core
+  (:require [konserve.memory :refer [new-mem-store]]
+            [replikativ.peer :refer [client-peer]]
+            [replikativ.stage :refer [create-stage! connect!
+                                      subscribe-crdts!]]
+
+            [hasch.core :refer [uuid]]
+            
+            [replikativ.crdt.ormap.realize :refer [stream-into-identity!]]
+            [replikativ.crdt.ormap.stage :as s]
+            
+            [cljs.core.async :refer [>! chan timeout]]
+            [superv.async :refer [S] :as sasync]
+            [cljsjs.material-ui] ;; TODO why?
+            [om.next :as om :refer-macros [defui] :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs-react-material-ui.core :as ui]
+            [cljs-react-material-ui.icons :as ic]
+            [sablono.core :as html :refer-macros [html]]
+            [cljs-react-material-ui.core :as ui]
+            [cljs-react-material-ui.icons :as ic])
+  (:require-macros [superv.async :refer [go-try <? go-loop-try]]
+                   [cljs.core.async.macros :refer [go-loop]]))
+
+(enable-console-print!)
